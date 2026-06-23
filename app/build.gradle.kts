b2dcb87021341378c1d66c3e2b14fe47d34157c6
User <muk.lisca@gmail.com>
2026-06-23 08:43:22 +0700

    chore(app): bump to v1.2 (versionCode 3) for update demo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.jni.videodownloader"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "BACKEND_BASE_URL", "\"$backendBaseUrl\"")
         buildConfigField("String", "BACKEND_API_KEY", "\"$backendApiKey\"")
